d6cc5f05abca5345b061a277f3a032d809521627
gynr <gprai.mail@gmail.com>
2025-08-23 16:24:26 +0530

    Add interface for chess boards and pieces.

---
+++ chess/interface/board.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Board(ABC):
+
+    @abstractmethod
+    def notation_to_cell_coordinates(self, notation: str) -> tuple[int, int]:
+        """
+        Convert a chess notation to cell coordinates.
+        Example for 8x8 board:
+        - A1 -> (0, 0)
+        - H8 -> (7, 7)
+        """
+        pass
+
+    @abstractmethod
+    def cell_coordinates_to_notation(self, cell_coordinates: tuple[int, int]) -> str:
+        """
+        Convert a cell coordinates to chess notation.
+        Example for 8x8 board:
+        - (0, 0) -> A1
+        - (7, 7) -> H8
+        """
+        pass
+
+    @abstractmethod
+    def is_valid_position(self, position: tuple[int, int]) -> bool:
+        """
+        Check if a position is valid on the board.
+        """
+        pass
+
+    
